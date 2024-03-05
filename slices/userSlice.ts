37ce62ebb752a62883e384f1af3968345dcee62e
Sarah Reimann <reimannsarah45@gmail.com>
2024-03-05 14:57:14 -0800

    add userSlice slice of state

---
+++ slices/userSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userName: ""
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUserName: (state, action) => {
+      state.userName = action.payload;
+    }
+  }
+});
+
+export const { setUserName } = userSlice.actions;
+export default userSlice.reducer;
